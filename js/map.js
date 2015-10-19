32d8585dbd96d79cceb349da188c3483a5a21a0f
Chris Leatham <chris13@uw.edu>
2015-10-18 23:12:48 -0700

    I think I am done...

--- js/map.js
+++ js/map.js
@@ -1,4 +1,22 @@
-// Gender variables.
+// Chris Leatham
+// Mike Freeman
+// INFO 343 A
+
+
+// JavaScript for creating a map using MapBox, taking data from a JSON file, plotting points based on variables, adding up statistics, then sending them to the HTML Table. 
+
+
+// Initializing map and its layers.
+var map;
+var unknownGLayer = new L.LayerGroup([]);
+var maleLayer = new L.LayerGroup([]);
+var femaleLayer = new L.LayerGroup([]);
+var allLayers = {
+    "Gender: Male": maleLayer,
+    "Gender: Female": femaleLayer,
+    "Gender: Unknown": unknownGLayer
+}
+
 var hitMale = 0;
 var killedMale = 0;
 var hitFemale = 0;
@@ -6,24 +24,14 @@ var killedFemale = 0;
 var hitUnknown = 0;
 var killedUnknown = 0;
 
-// The Layers
-var map;
-var maleLayer = new L.LayerGroup([]);
-var femaleLayer = new L.LayerGroup([]);
-var unknownGLayer = new L.LayerGroup([]);
-var allLayers = {
-    "Gender: Male": maleLayer,
-    "Gender: Female": femaleLayer,
-    "Gender: Unknown": unknownGLayer
-}
-
 // drawMap
-
 var drawMap = function() {
-
-    var map = L.map("container").setView([30, -100], 4);
-    var layer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png");
-    layer.addTo(map);
+    var tilesLayer = L.tileLayer("https://api.mapbox.com/v4/mapbox.streets-satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hyaXNsZWF0aGFtMTMiLCJhIjoiY2lmc2xqeW1sMWUzcWxma3JnMTl5ejA0cCJ9.fa6sIq_E4aJX-uOlpF-HqA");
+    map = L.map("container", {
+        center: [39.875, -98.6546],
+        zoom: 3
+    });
+    
 
     tilesLayer.addTo(map);
     getData();
@@ -44,103 +52,92 @@ var getData = function() {
     });
 }
 
-// Loops through the data and plots points
+// Loops through the data and determines what the victims qualities are depending on 
+// what happened.
 var customBuild = function(data) {
     for (var i = 0; i < data.length; i++) {
 
-        // Location
+        // Location of shooting (latitude and longitude)
         var lat = data[i]["lat"];
         var lng = data[i]["lng"];
 
-        // Name
-        var victimName = data[i]["Victim Name"];
-        if (victimName == "undefined") {
-            victimName = "Unknown";
+        // The name of the victim
+        var Name = data[i]["Victim Name"];
+        if (Name == "undefined") {
+            Name = "Unknown";
         }
 
-        // Gender
-        var victimGender = data[i]["Victim's Gender"];
-        if (victimGender != "Male" && victimGender != "Female") {
-            victimGender = "Unknown";
+        // The gender of the victim
+        var Gender = data[i]["Victim's Gender"];
+        if (Gender != "Male" && Gender != "Female") {
+            Gender = "Unknown";
         }
 
-        // Race
-        var victimRace = data[i]["Race"];
-        if (victimRace == "undefined") {
-            victimRace = "Unknown";
+        // The race of the victim
+        var Race = data[i]["Race"];
+        if (Race == "undefined") {
+            Race = "Unknown";
         }
 
-        // Result
-        var victimSurvive = data[i]["Hit or Killed?"];
-        if (victimSurvive == "undefined") {
-            victimSurvive = "Unknown";
+        // What the result of the shooting is 
+        var Survive = data[i]["Hit or Killed?"];
+        if (Survive == "undefined") {
+            Survive = "Unknown";
         }
 
-        // Description and link
-        var victimSummary = data[i]["Summary"];
-        if (victimSummary == "undefined") {
-            victimSummary = "Summary unknown.";
+        // Tells whether a summary of the shooting is available
+        var Summary = data[i]["Summary"];
+        if (Summary == "undefined") {
+            Summary = "No summary known";
         }
-        var victimSource = data[i]["Source Link"];
 
-        // Counts results
-        if (victimGender == "Male") {
-            if (data[i]["Hit or Killed?"] == "Hit") {
-                hitMale++;
-            } else {
-                killedMale++;
-            }
-        } else if (victimGender == "Female") {
-            if (data[i]["Hit or Killed?"] == "Hit") {
-                hitFemale++;
-            } else {
-                killedFemale++;
-            }
+        //Determines the source link of the article for the shooting
+        var Source = data[i]["Source Link"];
+
+        // Add up each count of the variables
+        if (Gender == "Male") {
+            (data[i]["Hit or Killed?"] == "Killed") ? killedMale++ : hitMale++;
+        } else if (Gender == "Female") {
+            (data[i]["Hit or Killed?"] == "Killed") ? killedFemale++ : hitFemale++;
         } else {
-            if (data[i]["Hit or Killed?"] == "Hit") {
-                hitUnknown++;
-            } else {
-                killedUnknown++;
-            }
+            (data[i]["Hit or Killed?"] == "Killed") ? killedUnknown++ : hitUnknown++;
         }
 
-        // Popup
-        var info = "<h3>" + victimName + "</h3>" + "<p>Survived?: " + victimSurvive + "</p><p>Gender: " + victimGender + "</p><p>Race: " + victimRace + "<p>Summary: " + victimSummary + "</p><p>Source: " + "<a href='" + victimSource + "'>" + victimSource + "</a></p>";
-
-        // Popup site.
-        var infoPopup = new L.popup({
+        // Creates the popup and puts it in the location
+        var description = "<h3>" + Name + "</h3>" + "<p>Hit or Killed:  " + Survive + "</p><p>Gender: " + Gender + "</p><p>Race: " + Race + "<p>Summary: " + Summary + "</p><p>Source: " + "<a href='" + Source + "'>" + Source + "</a></p>";
+        var popup = new L.popup({
             maxHeight: 200
-        }).setLatLng(lat, lng).setContent(info);
+        }).setLatLng(lat, lng).setContent(description);
 
-        // Adds circle to map based on what happened at the site
-        if (victimGender == "Male") {
-            var circle = new L.circle([lat, lng], 10, {
-                color: blue,
-                fillColor: blue,
-                fillOpacity: 0.25,
-            }).addTo(maleLayer).bindPopup(infoPopup);
-        } else if (victimGender == "Female") {
-            var circle = new L.circle([lat, lng], 10, {
-                color: pink,
-                fillColor: pink,
-                fillOpacity: 0.25,
-            }).addTo(femaleLayer).bindPopup(infoPopup);
+        // Inserts circle at incident site, adds circle to genderLayer and binds to popup.
+        if (Gender == "Male") {
+            var circle = new L.circle([lat, lng], 500, {
+                color: "#87CEFA",
+                fillColor: "#87CEFA",
+                fillOpacity: 0.6,
+            }).addTo(maleLayer).bindPopup(popup);
+        } else if (Gender == "Female") {
+            var circle = new L.circle([lat, lng], 500, {
+                color: "#FFB6C1",
+                fillColor: "#FFB6C1",
+                fillOpacity: 0.6,
+            }).addTo(femaleLayer).bindPopup(popup);
         } else {
-            var circle = new L.circle([lat, lng], 10, {
-                color: gray,
-                fillColor: gray,
-                fillOpacity: 0.25,
-            }).addTo(unknownGLayer).bindPopup(infoPopup);
+            var circle = new L.circle([lat, lng], 500, {
+                color: "#D3D3D3",
+                fillColor: "#D3D3D3",
+                fillOpacity: 0.6,
+            }).addTo(unknownGLayer).bindPopup(popup);
         }
     }
 
-    // addTo map stuff
+    // Displaying layers.
     maleLayer.addTo(map);
     femaleLayer.addTo(map);
     unknownGLayer.addTo(map);
     L.control.layers(null, allLayers).addTo(map);
 
-    // Add to table
+    // Updating statistics of the table.
     $("#hitMaleTotal").text(hitMale);
     $("#killedMaleTotal").text(killedMale);
     $("#hitFemaleTotal").text(hitFemale);
